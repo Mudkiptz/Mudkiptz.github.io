2818f9ba31403113435575649ef43fe33bcdc855
Bryant Nguyen <bryantn.tomozaki@gmail.com>
2018-10-09 10:55:27 -0700

    added image
    working

---
+++ p5_exercise_1/sketch.js
@@ -0,0 +1,17 @@
+// declare variables here
+
+
+function setup() {
+  // put setup code here --> this runs once upon launch
+
+  createCanvas(500, 500);
+  background(0);
+}
+
+ console.log("hello art 75, i am printing to the console");
+function draw() {
+  // put drawing code here --> this loops every frame
+}
+
+
+// write custom functions here
